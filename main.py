743870a1215684e32bad93528fd1805ea1667d35
Carlos Nardi <37674568+carlosnardi@users.noreply.github.com>
2024-11-15 09:45:35 +0000

    Fixed a number of bugs and errors in the code.

--- main.py
+++ main.py
@@ -12,29 +12,34 @@ def compare_screen(num_a, num_b):
   vote = input("Who has more followers? Type 'A' or 'B': ").lower()
   return vote
 
-def compare_vote(num_a):  
+def compare_vote(score_points):  
   vote = compare_screen(num_a, num_b)
-  global score
   if vote == 'a' and followers_a > followers_b:
     a_or_b = 'a'
-    score += 1
-    print(f"You're right. Current score: {score}")
+    score_points += 1
+    print(f"You're right. Current score: {score_points}")
   elif vote == 'b' and followers_b > followers_a:
     a_or_b = 'b'
-    score += 1
-    print(f"You're right. Current score: {score}")
+    score_points += 1
+    print(f"You're right. Current score: {score_points}")
   else:
     a_or_b = 'c'
-    print(f"Sorry. That's wrong. Final score: {score}")
-  return a_or_b
+    print(f"Sorry. That's wrong. Final score: {score_points}")
+  return a_or_b, score_points
 
 score = 0
 a_or_b = 0
-num_a = randint(0,len(data))
-num_b = randint(0,len(data))
+num_a = randint(0,len(data)-1)
+num_b = randint(0,len(data)-1)
 followers_a = data[num_a]['follower_count']
 followers_b = data[num_b]['follower_count']
-while compare_vote(num_a) != 'c':
-  num_b = randint(0,len(data))
-  followers_b = data[num_b]['follower_count']
-  
+while True:
+  result, score = compare_vote(score)
+  if result == 'a':
+    num_b = randint(0,len(data)-1)
+    followers_b = data[num_b]['follower_count']
+  elif result == 'b':
+    num_a = randint(0,len(data)-1)
+    followers_a = data[num_a]['follower_count']
+  else: 
+    break
